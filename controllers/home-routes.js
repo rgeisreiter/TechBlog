fad00d8aad85fff13e640a7a374785f3a0b85a2d
rgeisreiter <rgeisreiter@yahoo.com>
2021-06-02 16:33:41 -0600

    finished signup and login routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,3 +34,21 @@ router.get("/post/:id", (req, res) => {
     }
   });
 });
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+  res.render("login");
+});
+
+module.exports = router;
